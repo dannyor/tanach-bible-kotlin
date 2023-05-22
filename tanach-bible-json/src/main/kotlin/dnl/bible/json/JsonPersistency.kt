fb6fa7f97c2898f0375bbef76c2575fdb59bdba3
dannyor <daniyel.orr@gmail.com>
2023-05-22 18:40:03 +0300

    update versions

--- tanach-bible-json/src/main/kotlin/dnl/bible/json/JsonPersistency.kt
+++ tanach-bible-json/src/main/kotlin/dnl/bible/json/JsonPersistency.kt
@@ -5,14 +5,16 @@ import kotlinx.serialization.json.Json
 import org.apache.commons.io.FileUtils
 import java.io.File
 
+private val json = Json { prettyPrint = true }
+
 class JsonPersistency {
     fun write(file: File, bible: SerializableBible) {
-        val encodedString = Json { prettyPrint = true }.encodeToString(bible)
+        val encodedString = json.encodeToString(bible)
         FileUtils.write(file, encodedString, "UTF-16")
     }
 
     fun write(file: File, book: SerializableBook) {
-        val encodedString = Json { prettyPrint = true }.encodeToString(book)
+        val encodedString = json.encodeToString(book)
         FileUtils.write(file, encodedString, "UTF-16")
     }
 }
