54bd1ecafd8ecdf9350eb94d099875357f74ca18
Daniyel Orr <daniyel.orr@gmail.com>
2024-04-17 17:21:30 +0300

    add tests

--- tanach-bible-api/src/test/kotlin/dnl/bible/api/CharacterIteratorTest.kt
+++ tanach-bible-api/src/test/kotlin/dnl/bible/api/CharacterIteratorTest.kt
@@ -3,10 +3,11 @@ package dnl.bible.api
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@Suppress("SpellCheckingInspection")
 class CharacterIteratorTest {
 
     @Test
-    fun test() {
+    fun testSingleVerse() {
         val verse:Verse = TestVerse("one two three")
         val iterator = CharacterIterator(listOf(verse).iterator())
         val sb = StringBuilder()
@@ -14,6 +15,21 @@ class CharacterIteratorTest {
             sb.append(it)
         }
         assertEquals("onetwothree", sb.toString())
+    }
+
+    @Test
+    fun testMultipleVerses() {
+        val verses = listOf(
+            TestVerse("one two three"),
+            TestVerse("four five six"),
+            TestVerse("seven eight nine")
+        )
+        val iterator = CharacterIterator(verses.iterator())
+        val sb = StringBuilder()
+        iterator.forEach {
+            sb.append(it)
+        }
+        assertEquals("onetwothreefourfivesixseveneightnine", sb.toString())
     }
 
 }
